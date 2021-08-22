323d2a0037543c9f6e66b21c8d56900f2ff8b2c2
bayuabi <bayuabi31@gmail.com>
2021-08-22 14:16:32 +0700

    update methate reading algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,11 @@ void sendCO2Val(int value);
 void sendMethaneVal(int value);
 float MGRead();
 void sendCurrentValveControl();
+float MQ8Calibration();
+float MQ8ResistanceCalculation(int raw_adc);
+float MQ8Read();
+int MQ8GetGasPercentage(float rs_ro_ratio, int gas_id);
+int  MQ8GetPercentage(float rs_ro_ratio, float *pcurve);
 
 #define TURN_RELAY_1_ON   digitalWrite(RELAY_1_PIN, LOW)
 #define TURN_RELAY_1_OFF  digitalWrite(RELAY_1_PIN, HIGH)
@@ -44,12 +49,25 @@ float CO2Curve[3] = {2.602,MG811_ZERO_POINT_VOLTAGE,(MG811_REACTION_VOLTAGE/(2.6
 #define MG811_READ_SAMPLE_TIMES     5
 #define MG811_READ_SAMPLE_INTERVAL  50
 
+#define MQ8_RL_VALUE                    (10)
+#define MQ8_RO_CLEAN_AIR_FACTOR         (9.21)
+#define MQ8_CALIBRATION_SAMPLE_TIMES    (50)
+#define MQ8_CALIBRATION_SAMPLE_INTERVAL (500)
+#define MQ8_READ_SAMPLE_TIMES           (5)
+#define MQ8_READ_SAMPLE_INTERVAL        (50)
+#define MQ8_GAS_H2                      (0)
+
+float H2Curve[3] = {2.3, 0.93, -1.44};
+float MQ8_Ro = 10;
+
 bool automaticValveControl = false;
 
 void setup() {
   Serial.begin(115200);
 
   pinInit();
+
+  MQ8_Ro = MQ8Calibration();  
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   Serial.print("Connecting");
@@ -90,9 +108,7 @@ void pinInit(){
 }
 
 int readHydrogen(){
-  float calVal = 0.1;
-  int adcValue = analogRead(MQ8_PIN);
-  int hydrogenVal = adcValue * calVal;
+  int hydrogenVal = MQ8GetGasPercentage(MQ8Read()/MQ8_Ro,MQ8_GAS_H2);
 
   return hydrogenVal;
 }
@@ -144,6 +160,56 @@ float MGRead(){
   mvValue = (mvValue/MG811_READ_SAMPLE_TIMES) * 3.3 / 4096;
 
   return mvValue;
+}
+
+float MQ8Calibration()
+{
+  int i;
+  float val=0;
+ 
+  for (i=0;i<MQ8_CALIBRATION_SAMPLE_TIMES;i++) {            //take multiple samples
+    val += MQ8ResistanceCalculation(analogRead(MQ8_PIN));
+    delay(MQ8_CALIBRATION_SAMPLE_INTERVAL);
+  }
+  val = val/MQ8_CALIBRATION_SAMPLE_TIMES;                   //calculate the average value
+ 
+  val = val/MQ8_RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro 
+                                                        //according to the chart in the datasheet 
+ 
+  return val; 
+}
+
+float MQ8ResistanceCalculation(int raw_adc)
+{
+  return ( ((float)MQ8_RL_VALUE*(4096-raw_adc)/raw_adc));
+}
+
+float MQ8Read()
+{
+  int i;
+  float rs=0;
+ 
+  for (i=0;i<MQ8_READ_SAMPLE_TIMES;i++) {
+    rs += MQ8ResistanceCalculation(analogRead(MQ8_PIN));
+    delay(MQ8_READ_SAMPLE_INTERVAL);
+  }
+ 
+  rs = rs/MQ8_READ_SAMPLE_TIMES;
+ 
+  return rs;  
+}
+ 
+int MQ8GetGasPercentage(float rs_ro_ratio, int gas_id)
+{
+  if ( gas_id == MQ8_GAS_H2) {
+     return MQ8GetPercentage(rs_ro_ratio,H2Curve);
+  }  
+  return 0;
+}
+ 
+int  MQ8GetPercentage(float rs_ro_ratio, float *pcurve)
+{
+  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
 }
 
 BLYNK_WRITE(V4){
